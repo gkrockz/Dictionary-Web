be157c9868d11321f4f0c5a5057a5fe0964022eb
gkrockz <61354468+gkrockz@users.noreply.github.com>
2021-10-12 16:31:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const result = document.getElementById("result");
+const voice = document.getElementById("sound");
+const searchBtn = document.getElementById("btn");
+var word = document.getElementById("word");
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+function playSound() {
+    voice.play();
+}
+
+function searchWord(word)  {
+    fetch(`${url}${word}`).then((response) => response.json()).then((data) => {
+            result.innerHTML = `
+                <div class="word">
+                    <h3>${word}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                    ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+         voice.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">OOPS, Couldn't find The Word !</h3>`;
+            console.clear();
+        });
+}
+
+// Event listener (for click and keypress)
+
+function EventForKeyPress(event) {
+    if (event.keyCode === 13) {
+      searchWord(word.value);
+    }
+  }
+
+searchBtn.addEventListener("click", () => {
+    searchWord(word.value);
+});
+
+word.addEventListener("keypress", EventForKeyPress);
